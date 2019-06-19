1672047578081587bdbe54fc1332908fc4358aad
iaminzmam <50803412+iaminzmam@users.noreply.github.com>
2019-06-19 12:56:16 +0530

    use your firebase api

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDyymSIlQOwYhzEE-rx_o6a9Y1x7BraVrE",
-    authDomain: "postbook-8b1ad.firebaseapp.com",
-    databaseURL: "https://postbook-8b1ad.firebaseio.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
     projectId: "postbook-8b1ad",
-    storageBucket: "postbook-8b1ad.appspot.com",
-    messagingSenderId: "127239365579",
-    appId: "1:127239365579:web:4a952ad48b9c2470"
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 export default firebaseConfig
